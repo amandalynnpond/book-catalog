f13756124543a3faaab711db014e6ecb30a14c55
Amanda Pond <thatotherpond@gmail.com>
2023-08-04 17:20:26 -0400

    Very slight stilization to better differentate books

--- src/components/BookInfo.js
+++ src/components/BookInfo.js
@@ -9,14 +9,14 @@ function BookInfo({book}){
         bookImages.forEach((image) => {
             const imageURI = image.uri
             if (imageURI.includes("jacket_cover")){
-                jacketCover = <img src={imageURI} width={300}/>
+                jacketCover = <img src={imageURI} width={300} alt={book.name} />
             }
         })
     }
 
 
     return(
-        <div>
+        <div className="border border-primary-subtle m-3 p-5">
             <h4>{book.name} by {book.author}</h4>
             {jacketCover}
         </div>
